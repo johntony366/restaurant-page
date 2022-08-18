369a1162ec08ce512d8c89965b45129a145dc7d8
John <johntony366@gmail.com>
2022-08-18 13:31:24 +0530

    Finish loadContact.js

--- src/js/loadContact.js
+++ src/js/loadContact.js
@@ -1,1 +1,24 @@
-export default function loadContact() {}
+import { createDiv } from './initWebsite';
+import wineClink from '/src/assets/wine-clink.jpg';
+
+export default function loadContact() {
+    const main = document.querySelector("main");
+    main.replaceChildren(); //Deleting all current children
+
+    main.setAttribute("id", "contact");
+
+    const contactContainer = createDiv('contact-container');
+    const callP = document.createElement('p');
+    callP.textContent = "📞 Call us at +91 98765 43 210";
+    const mailP = document.createElement('p');
+    mailP.textContent = "✉️ Mail us at realrestaurant@gmail.com";
+    const contactImg = document.createElement('img');
+    contactImg.src = wineClink;
+    contactImg.alt = "Two wine glasses being clinked"
+
+    contactContainer.appendChild(callP);
+    contactContainer.appendChild(mailP);
+    contactContainer.appendChild(contactImg);
+
+    main.appendChild(contactContainer);
+}
